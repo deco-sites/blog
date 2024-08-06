9ab12ca275ca5a4696438acc242715d22dae66e2
guitavano <tavano62@gmail.com>
2024-08-06 08:02:00 -0300

    update site.ts

--- apps/site.ts
+++ apps/site.ts
@@ -23,7 +23,10 @@ export default function Site(
     state,
     manifest,
     dependencies: [
-      website(state),
+      website({
+        ...state,
+        global: state.theme ? [...(state.global ?? []), state.theme] : state.global,
+      }),
       blog(state),
     ],
   };
